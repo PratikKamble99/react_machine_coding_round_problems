13a6b70435675589cd58009f6c0314a7954c07b7
Pratik Kamble <pratik.kamble@shwaira.com>
2026-01-21 18:08:14 +0530

    Changed Debounce API call component location

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import DigitalClock from "./problems/beginner/DigitalClock";
 import GoogleSheet from "./problems/expert/googleSheet/GoogleSheet";
 import ApiRetry from "./problems/intermediate/api_retry_and_cancellation/ApiRetry";
-import DebounceApiCall from "./problems/intermediate/DebounceApiCall";
+import DebounceApiCall from "./problems/intermediate/debounce_api_call/DebounceApiCall";
 import Tabs from "./problems/intermediate/tabs_component/Tabs";
 import ToastProvider from "./problems/intermediate/toast_notification/ToastContext";
 import Toast from "./problems/intermediate/toast_notification/Toast";
